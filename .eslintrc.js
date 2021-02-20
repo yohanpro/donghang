e2f88eb8c80823c12529147dea6c962e35fc258a
Yohan <yohan@yohanpro.com>
2021-02-20 12:06:11 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  extends: [
+    '@nuxtjs',
+    // 'prettier',
+    // 'prettier/vue',
+    // 'plugin:prettier/recommended',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: ['prettier'],
+  // add your custom rules here
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+    'quote-props': ['error', 'consistent'],
+    'vue/max-attributes-per-line': [
+      'error',
+      {
+        singleline: 1,
+        multiline: {
+          max: 1,
+          allowFirstLine: false,
+        },
+      },
+    ],
+  },
+}
