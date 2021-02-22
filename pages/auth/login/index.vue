f2ce846bc1b6683342f7b6f36b37b222235ed47b
Yohan <yohan@yohanpro.com>
2021-02-22 21:28:47 +0900

    feat: login 페이지 기본 마크업

---
+++ pages/auth/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <SignUpGate />
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+  components: {
+    SignUpGate: () => import('~/components/auth/sign-up'),
+  },
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ pages/auth/sign-up/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    회원가입 페이지입니다.
+    <h1>This is SignUP Page</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'SignupPage',
+}
+</script>
