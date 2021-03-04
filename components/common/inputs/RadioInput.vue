6256bbf0e1045803ff527069b7a4f6d342028c9c
Yohan <yohan@yohanpro.com>
2021-03-04 21:15:11 +0900

    feat: 전화번호 이메일, 성별 입력 구현

---
+++ components/common/inputs/RadioInput.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="button-select-wrapper">
+    <button
+      v-for="selectField in selectArray"
+      :key="selectField.value"
+      :class="[{selected: selectedField === selectField }, { invalid: isInvalid }]"
+      @click="handleSelect(selectField)"
+    >
+      {{ selectField.placeholder }}
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'RadioInput',
+  props: {
+    selectArray: {
+      type: Array,
+      required: true,
+    },
+    isInvalid: {
+      type: Boolean,
+      default: false,
+    },
+    selectedValue: {
+      type: [Number, String],
+      default: '',
+    },
+  },
+  data () {
+    return {
+      selectedField: '',
+    }
+  },
+  created () {
+    this.selectedField = this.selectArray.filter((item) => { return item.value === this.selectedValue })[0]
+  },
+  methods: {
+    handleSelect (selectField) {
+      this.selectedField = selectField
+      this.$emit('inputSelect', selectField)
+    },
+    getSelectedIndex () {
+      return this.selectArray.map((item) => { return item.value }).indexOf(this.selectedValue)
+    },
+  },
+}
+</script>
+<style lang="scss" scoped>
+.button-select-wrapper {
+  display: flex;
+  flex-grow: 1;
+  margin: 0 6px;
+}
+button {
+  margin: 0 6px;
+  background-color: #fff;
+  border: 1px solid var(--very-light-pink);
+  border-radius: 4px;
+  padding: 10px 12px;
+  flex: 1;
+  color: #999;
+  text-align: center;
+}
+
+.invalid {
+  border: 1px solid #f86884;
+}
+
+.selected {
+  background: var(--tealish);
+  color: #fff;
+}
+</style>
