1ccbea5e987859c7db1d143b7d38553d3bb65472
Yohan Kim <yohan@yohanpro.com>
2021-03-08 23:05:36 +0900

    feat: 회원가입 버튼  disable 구현, 클릭 시 회원가입 축하페이지 이동 구현 feat: 날짜 연도 , 달을 선택시 date-fns에서 해당 연월에 맞는 일자수 업데이트 구현

---
+++ pages/auth/sign-up/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <base-header
+      is-back-button
+      header-title="회원가입을 환영합니다"
+      @close="$router.push('/')"
+    />
+    회원가입을 환영합니다~
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WelcomePage',
+  components: {
+    BaseHeader: () => import('~/components/common/header/BaseHeader'),
+  },
+  layout: 'with-header',
+}
+</script>
