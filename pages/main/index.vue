7d488f7121442eb257c536260982bfd6d3ae6cd7
Yohan <yohan@yohanpro.com>
2021-03-10 23:29:27 +0900

    Feat: Main Page, Main Component init

---
+++ pages/main/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Main />
+</template>
+<script>
+
+export default {
+  name: 'MainPage',
+  components: {
+    Main: () => import('~/components/main'),
+  },
+  layout: 'with-header',
+}
+
+</script>
