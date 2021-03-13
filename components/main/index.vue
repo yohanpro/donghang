a42b0e07c394fe6fc63caa1024807076868eeda4
Yohan <yohan@yohanpro.com>
2021-03-13 12:12:18 +0900

    feat: 하단 내비게이션 탭 구현 style: 스포카 한산스 적용

--- components/main/index.vue
+++ components/main/index.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-    MainComponent
+    <base-header
+      header-title="홈"
+      is-back-button
+    />
+    <MainArticles />
+    <BottomBar />
   </div>
 </template>
+
 <script>
 export default {
   name: 'Main',
-
+  components: {
+    BaseHeader: () => import('~/components/common/header/BaseHeader'),
+    BottomBar: () => import('~/components/common/bottom-bar'),
+    MainArticles: () => import('./MainArticles'),
+  },
 }
 </script>
