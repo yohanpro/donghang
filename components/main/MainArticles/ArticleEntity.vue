0dbf254f85a2b8bb189663379c40eb0d6ee275fb
Yohan Kim <yohan@yohanpro.com>
2021-03-13 20:43:13 +0900

    Feature/filters 필터, 게시글 기본 마크업 구현 (#10)
    * feat: 하단 내비게이션 탭 구현
    style: 스포카 한산스 적용
    
    * Feat: 필터, 게시글 기본 마크업

---
+++ components/main/MainArticles/ArticleEntity.vue
@@ -0,0 +1,34 @@
+<template>
+  <article class="article">
+    <p class="article__title">
+      {{ articleObj.title }}
+    </p>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'ArticleEntity',
+  props: {
+    articleObj: {
+      type: Object,
+      default: () => {},
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.article {
+  width: 100%;
+  height: 13rem;
+  border-bottom: var(--border-light-gray);
+  background-color: #fff;
+  padding: 1rem 1.5rem;
+
+  .article__title {
+    font-size: 1.5rem;
+    font-weight: bold;
+  }
+}
+</style>
