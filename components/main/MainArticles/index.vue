e50dae6c83dc2edeb777d6beb3015562fc7dabdf
Yohan <yohan@yohanpro.com>
2021-03-13 12:12:18 +0900

    feat: 하단 내비게이션 탭 구현 style: 스포카 한산스 적용

---
+++ components/main/MainArticles/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section id="main-articles">
+    메인 글들
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MainArticles',
+}
+</script>
+
+<style lang="scss" scoped>
+#main-articles {
+  width: 100%;
+  height: 200vh;
+}
+</style>
