e0d743139a606874e9f56ff1e3dc8cd805d3621e
Yohan <yohan@yohanpro.com>
2021-03-20 15:54:20 +0900

    Feat :  프로필 기본, detail 페이지, 참여희망자 기본 마크업

---
+++ components/main/detail/contents-area/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <section class="contents-area__container">
+    <div class="contents-area__main-text">
+      {{ detailObj.content }}
+    </div>
+    <div class="contents-area__address">
+      <p>장소</p>
+      <p>{{ detailObj.address }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'ContentsArea',
+  props: {
+    detailObj: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data () {
+    return {}
+  },
+}
+</script>
+<style lang="scss" scoped>
+.contents-area {
+  &__container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+  }
+  &__main-text {
+    padding: 1rem 2rem;
+    font-size: 1.6rem;
+    white-space: pre-line;
+    word-break: break-word;
+  }
+  &__address {
+    padding: 1rem 2rem;
+    font-size: 1.6rem;
+    flex-wrap: nowrap;
+
+    p {
+      font-weight: 300;
+    }
+    p:first-child {
+      font-weight: bold;
+    }
+  }
+}
+</style>
---
+++ components/main/detail/elements/TagArea.vue
@@ -0,0 +1,52 @@
+<template>
+  <section class="tag-area">
+    <p
+      v-for="tag in concatedTags"
+      :key="tag"
+    >
+      # {{ tag }}
+    </p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'TagArea',
+  props: {
+    tags: {
+      type: Array,
+      required: true,
+    },
+    isOnlyWoman: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data () {
+    return {
+      concatedTags: [],
+    }
+  },
+  created () {
+    this.concatedTags = this.tags
+    if (this.isOnlyWoman) {
+      this.concatedTags.push('여자만')
+    }
+  },
+}
+</script>
+<style lang="scss" scoped>
+.tag-area {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  p {
+    color: rebeccapurple;
+    font-weight: 500;
+    font-size: 1.2rem;
+    &:not(:last-child) {
+      margin-right: 1rem;
+    }
+  }
+}
+</style>
