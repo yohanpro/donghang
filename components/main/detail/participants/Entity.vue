3f76ef0de119960cedbd93094d351ba8991aa985
Yohan <yohan@yohanpro.com>
2021-04-10 20:14:28 +0900

    Feat: Contents area detail 작업

---
+++ components/main/detail/participants/Entity.vue
@@ -0,0 +1,115 @@
+<template>
+  <v-card
+    elevation="2"
+    outlined
+    shaped
+    tile
+    class="participant"
+  >
+    <button
+      v-show="isCardOwner"
+      class="button-delete-card"
+    >
+      <img
+        src="~/assets/images/icons/trashcan.svg"
+        alt="Delete"
+      >
+    </button>
+    <div class="participant__wrapper">
+      <div class="participant__profile">
+        <img
+          :src="participant.profile.imageUrl"
+          alt=""
+        >
+      </div>
+      <div class="participant__detail">
+        <div class="participant__detail__text">
+          {{ participant.detail.textarea }}
+        </div>
+      </div>
+    </div>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'ParticipantsEntity',
+  props: {
+    participant: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  data () {
+    return {
+      auth: {
+        name: 'john',
+        id: 1232313,
+      },
+    }
+  },
+  computed: {
+    isCardOwner () {
+      return this.participant.id === this.auth.id
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.participant {
+  width: 100%;
+  height: 120px;
+  margin-bottom: 2rem;
+  position: relative;
+  border-radius: 10px !important;
+  &__wrapper {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    display: flex;
+    align-items: center;
+  }
+  &__profile {
+    width: 25%;
+    flex-basis: 25%;
+    max-width: 150px;
+    height: 100%;
+    overflow: hidden;
+    margin-left: 1rem;
+    display: flex;
+    align-items: center;
+    > img {
+      width: 100%;
+      border-radius: 50%;
+    }
+  }
+  &__detail {
+    display: flex;
+
+    flex-direction: column;
+    flex: 1;
+    height: 100%;
+    padding: 1rem 1.5rem;
+  }
+}
+
+.button-delete-card {
+  width: 2.5rem;
+  height: 2.5rem;
+  position: absolute;
+  right: -10px;
+  top: -10px;
+  overflow: hidden;
+  background-color: #eee;
+  border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid #ccc;
+  > img {
+    width: 80%;
+    height: auto;
+  }
+}
+</style>
--- components/main/detail/participants/index.vue
+++ components/main/detail/participants/index.vue
@@ -1,21 +1,92 @@
 <template>
   <section class="participants-area-container">
-    참여희망자 에어리어
+    <div
+      v-show="!isOwner"
+      class="participants-area__button-container"
+    >
+      <v-btn
+        depressed
+        height="30"
+        width="100%"
+        color="primary"
+      >
+        참여 희망
+      </v-btn>
+      <button />
+    </div>
+    <div class="participants-area__list-container">
+      <ParticipantsEntity
+        v-for="participant in participantsArr"
+        :key="participant.id"
+        :participant="participant"
+      />
+    </div>
   </section>
 </template>
 
 <script>
 export default {
   name: 'ParticipantsArea',
+  components: {
+    ParticipantsEntity: () => import('./Entity'),
+  },
+  data () {
+    return {
+      isOwner: false,
+      participantsArr: [
+        {
+          id: 1232313,
+          email: 'bravo@abcd.com',
+          nickname: '고민고민중',
+          profile: {
+            imageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQEZrATmgHOi5ls0YCCQBTkocia_atSw0X-Q&usqp=CAU',
+          },
+          detail: {
+            textarea: '안녕하세요 저도 반갑습니다.',
+          },
+        },
+        {
+          id: 2222,
+          email: 'talent@example.com',
+          nickname: '복면가왕',
+          profile: {
+            imageUrl: 'http://thenewcode.com/assets/images/thumbnails/sarah-parmenter.jpeg',
+          },
+          detail: {
+            textarea: '어 저도 그 때 시간이 맞아요',
+          },
+        },
+      ],
+    }
+  },
 }
 </script>
 
 <style lang="scss" scoped>
+::v-deep .v-btn {
+  height: 2rem;
+}
 .participants-area {
   &-container {
     width: 100%;
     height: 60vh;
     padding: 1rem 2rem;
+    display: flex;
+    flex-direction: column;
+  }
+  &__button-container {
+    width: 100%;
+    border: 1px solid #eeeeee;
+    border-radius: 7px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  &__list-container {
+    display: flex;
+    flex-direction: column;
+    padding: 1rem 0 10rem;
+    margin-bottom: 2rem;
   }
 }
 </style>
