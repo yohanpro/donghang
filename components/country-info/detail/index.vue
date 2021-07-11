096fb1c137821c196a1c4ea8bf424aa3d55f844d
Yohan <yohan@yohanpro.com>
2021-07-11 22:37:34 +0900

    Feat:Country Info detail 페이지, 컴포넌트 생성

---
+++ components/country-info/detail/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="country-info-detail">
+    <div>country Info detail Component</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountryInfoDetail',
+}
+</script>
---
+++ pages/country-info/_countryId/_posterId/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <CountryInfoDetail />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CountryInfoDetailPage',
+  components: {
+    CountryInfoDetail: () => import('~/components/country-info/detail/index'),
+  },
+}
+</script>
